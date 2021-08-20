6b7899409bbc8d5aaa1c671ee5e0a8aaf12c10fa
sprabh143 <38618144+sprabh143@users.noreply.github.com>
2021-08-20 04:15:22 -0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Topbar from "./Components/topbar/Topbar";
+import Intro from "./Components/intro/Intro";
+import Portfolio from "./Components/portfolio/Portfolio";
+import Works from "./Components/works/Works";
+import Testimonials from "./Components/testimonials/Testimonials";
+import Contact from "./Components/contact/Contact";
+import Menu from "./Components/menu/Menu";
+import "./app.scss";
+import { useState } from "react";
+//import { Menu } from "@material-ui/core";
+
+function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <div className="app">
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <div className="sections">
+        <Intro />
+        <Portfolio />
+        <Works />
+        <Testimonials />
+        <Contact />
+      </div>
+    </div>
+  );
+}
+
+export default App;
